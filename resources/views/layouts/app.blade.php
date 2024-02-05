b882c6f92a99cd34030be9bc15a63d2caf8d286a
raffaelepiscitelli <raffaelepiscitelli1994@gmail.com>
2024-02-05 16:51:40 +0100

    Modifiche template

--- resources/views/layouts/app.blade.php
+++ resources/views/layouts/app.blade.php
@@ -1,12 +1,25 @@
 <!DOCTYPE html>
 <html lang="en">
+
 <head>
     <meta charset="UTF-8">
     <meta name="viewport" content="width=device-width, initial-scale=1.0">
     <title>Document</title>
     @vite('resources/js/app.js')
 </head>
+
 <body>
+
+    @include('partials.header')
+
+    <main>
+
+        @yield('main-content')
     
+    </main>
+
+    @include('partials.footer')
+
 </body>
+
 </html>
---
+++ resources/views/pages/home.blade.php
@@ -0,0 +1,5 @@
+@extends ('layouts.app')
+
+@section ('main-content')
+        <h1>Hello, World!</h1>
+@endsection
